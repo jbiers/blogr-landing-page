2ce8f37c200152cb93f6e68762cd7ff8759d603a
jbiers <juliabiersuriano@gmail.com>
2022-02-16 01:13:06 -0400

    finished mobile version

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-let hamburguer = document.getElementsByClassName('hamburguer')[0];
+const hamburger = document.querySelector('.hamburger-menu');
+const close = document.querySelector('.btn-close');
+const menu = document.querySelector('.menu');
 
-hamburguer.addEventListener('click', () => {
-    console.log("click");
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+    hamburger.classList.toggle('hamburger-hidden');
 });
+
+close.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+    hamburger.classList.toggle('hamburger-hidden');
+})
